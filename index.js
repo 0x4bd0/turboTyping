bbfc25b3ae84973f6bd2bbe33b9c83d7e2b8829d
0x4bd0 <abdoumjr@gmail.com>
2020-01-02 10:31:33 +0100

    50%

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const QUOTES_API = "https://api.quotable.io/random"
 const textArea = document.getElementById('text')
 const typingArea = document.getElementById('textType')
+const timer = document.getElementById('time')
 
 
 typingArea.addEventListener('input',() => {
    const quoteObject =  textArea.querySelectorAll('span')
    const typedObject = typingArea.value.split('')
+   let correct = true
    quoteObject.forEach((item,i)=>{
-    if (typedObject[i] === item.innerText) {
+       if(typedObject[i] == null) {
+            item.classList.remove('correct')
+            item.classList.remove('incorrect')
+            correct = false
+       }
+        else if (typedObject[i] === item.innerText) {
             item.classList.add('correct')
             item.classList.remove('incorrect')
         } else {
             item.classList.add('incorrect')
-            item.classList.add('correct')
+            item.classList.remove('correct')
+            correct = false
         } 
    })
+   if (correct) renderQuote()
 })
 
 const getQuote = () => {
@@ -26,13 +35,26 @@ const getQuote = () => {
 const renderQuote = async() =>{
     let quote = await getQuote()
     textArea.innerHTML = ''
+    typingArea.value = null
     quote.split('').forEach(item => {
      const mySpan = document.createElement('span')
      mySpan.innerText = item
      textArea.appendChild(mySpan)
     });
-    typingArea.innerText = null
 }
 
+let startTime
+
+const timer = () => {
+    timer.innerText = 0
+    startTime = new Date()
+    setInterval(()=>{
+     updateTimer()
+    },1000)
+}
+
+const updateTimer = () => {
+
+}
 
 renderQuote()
